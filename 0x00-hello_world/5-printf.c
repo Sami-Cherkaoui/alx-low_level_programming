3c18d4a6f8444fee70c2ca2b22018f65fbee71b4
Sami <cherkaoui.samii@gmail.com>
2023-07-13 22:33:37 +0100

    t

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #nclude <stdio.h>
-    int main(void)
-    {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
-    }
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
