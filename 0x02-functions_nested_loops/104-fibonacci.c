6a80cc0e2158c4d0c120ae6a573bc6717391495c
Sami <cherkaoui.samii@gmail.com>
2023-07-18 23:44:19 +0100

    t

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,63 @@
 #include <stdio.h>
 
-void fibonacci_sequence() {
-int a = 1;
-int b = 2;
-int count = 0;
+#define MAX_DIGITS 500
 
-printf("%d, %d, ", a, b);
-count += 2;
+void initialize_array(int array[], int value, int length) {
+for (int i = 0; i < length; i++) {
+array[i] = value;
+}
+}
 
-while (count < 98) {
-int next = a + b;
-printf("%d, ", next);
-count++;
+void add_arrays(int result[], int array1[], int array2[], int length) {
+int carry = 0;
+for (int i = 0; i < length; i++) {
+int sum = array1[i] + array2[i] + carry;
+result[i] = sum % 10;
+carry = sum / 10;
+}
+}
 
-a = b;
-b = next;
+void copy_array(int dest[], int src[], int length) {
+for (int i = 0; i < length; i++) {
+dest[i] = src[i];
+}
+}
+
+void print_fibonacci_sequence() {
+int a[MAX_DIGITS];
+int b[MAX_DIGITS];
+int result[MAX_DIGITS];
+
+initialize_array(a, 0, MAX_DIGITS);
+initialize_array(b, 0, MAX_DIGITS);
+initialize_array(result, 0, MAX_DIGITS);
+
+a[MAX_DIGITS - 1] = 1;
+b[MAX_DIGITS - 1] = 2;
+
+printf("%d, %d, ", a[MAX_DIGITS - 1], b[MAX_DIGITS - 1]);
+
+for (int i = 2; i < 98; i++) {
+add_arrays(result, a, b, MAX_DIGITS);
+copy_array(a, b, MAX_DIGITS);
+copy_array(b, result, MAX_DIGITS);
+
+int j = 0;
+while (result[j] == 0) {
+j++;
+}
+
+for (; j < MAX_DIGITS; j++) {
+printf("%d", result[j]);
+}
+printf(", ");
 }
 
 printf("\n");
 }
 
 int main() {
-fibonacci_sequence();
+print_fibonacci_sequence();
 return 0;
 }
 
