9e26d93200c80e58e89469694c2a0d6e1088026f
Sami <cherkaoui.samii@gmail.com>
2023-07-18 22:10:03 +0100

    t

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-/**
-*print _putchar
-*/
+
 int main(void) 
+/**
+* main - Entry point
+* Description: prints '_putchar\n'
+* Return: Always 0 (success)t add .;git commit -m 't';git push
+
+*/
 {
 printf("_putchar\n");
 return (0);
