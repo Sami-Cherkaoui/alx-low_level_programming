f15671fea8ff1caeb0b94ffdfc3be467dd02a3c0
Sami <cherkaoui.samii@gmail.com>
2023-07-18 23:57:21 +0100

    t

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 void fibonacci_sequence() {
-int a = 1;
-int b = 2;
-int count = 0;
+long int a = 1;
+long int b = 2;
+long int count = 0;
 
 printf("%d, %d, ", a, b);
 count += 2;
 
 while (count < 98) {
-int next = a + b;
+long int next = a + b;
 printf("%d, ", next);
 count++;
 
