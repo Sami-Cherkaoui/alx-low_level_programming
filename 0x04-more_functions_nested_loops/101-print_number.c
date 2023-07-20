b68e5ca0a1aa49a323f0396d2d3a46ae59d020db
Sami <cherkaoui.samii@gmail.com>
2023-07-20 19:00:07 +0100

    t

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,18 +5,21 @@
 * @n: int type number
 * Description: Can only use _putchar to print.
 */
+
 void print_number(int n)
 {
 long m; /* power of 10 */
 int c; /* boolean check */
 long num; /* convert int to long */
 num = n;
+
 /* negatives */
 if (num < 0)
 {
 num *= -1;
 _putchar('-');
 }
+
 /* count up */
 m = 1;
 c = 1;
@@ -27,6 +30,7 @@ m *= 10;
 else
 c = 0;
 }
+
 /* count down */
 while (num >= 0)
 {
