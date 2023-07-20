5b0871a3c6c7f6c7421c261ad2f83a8cd88c1d85
Sami <cherkaoui.samii@gmail.com>
2023-07-20 18:47:36 +0100

    t

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,20 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
-* print_number - Prints an integer.
-* @n: The integer to be printed.
-*/
-void print_number(int n)
+int main(void)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
+int number1 = 123;
+int number2 = -456;
 
-if (n / 10 != 0)
-print_number(n / 10);
+printf("Number 1: ");
+print_number(number1);
+putchar('\n');
 
-_putchar(n % 10 + '0');
+printf("Number 2: ");
+print_number(number2);
+putchar('\n');
+
+return 0;
 }
 
